dd0edd70f017e289d5be94b6b7c1a1cda7a14052
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-09 11:34:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-serial.redirectToUSB()
-let D1 = 0
-let D2 = 0
 let D3 = 0
+let D2 = 0
+let D1 = 0
+serial.redirectToUSB()
 let speed = 300
 cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 70)
-cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Stop, 0)
+cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
 basic.showIcon(IconNames.Happy)
 while (!(input.buttonIsPressed(Button.A))) {
-    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Stop, 0)
+    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
 }
 basic.showIcon(IconNames.Heart)
 basic.showLeds(`
@@ -21,50 +21,50 @@ basic.forever(function () {
 	
 })
 basic.forever(function () {
-    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Stop, 0)
-    basic.pause(100)
+    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 0)
+    basic.pause(500)
     cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 70)
-    basic.pause(200)
+    basic.pause(500)
     D1 = Math.floor(cbit_小车类.Ultrasonic_Car())
     while (D1 == 0) {
         D1 = Math.floor(cbit_小车类.Ultrasonic_Car())
     }
     cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 135)
-    basic.pause(200)
+    basic.pause(500)
     D2 = Math.floor(cbit_小车类.Ultrasonic_Car())
     while (D2 == 0) {
         D2 = Math.floor(cbit_小车类.Ultrasonic_Car())
     }
-    cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 25)
-    basic.pause(200)
+    cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 20)
+    basic.pause(500)
     D3 = Math.floor(cbit_小车类.Ultrasonic_Car())
     while (D3 == 0) {
         D3 = Math.floor(cbit_小车类.Ultrasonic_Car())
     }
     cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 70)
-    basic.pause(200)
+    basic.pause(500)
     if (D1 < 20 && D3 - D1 > 0) {
         speed = speed - 50
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Right, speed)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
         speed = 300
     } else if (D3 < 20 && D1 - D3 > 0) {
         speed = speed - 50
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Left, speed)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
         speed = 300
     } else if (D2 < 15) {
-        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, speed)
-        basic.pause(200)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
+        basic.pause(500)
         if (D3 - D1 > 0) {
             speed = speed - 50
-            cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Right, speed)
+            cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
             speed = 300
         } else {
             speed = speed - 50
-            cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Left, speed)
+            cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
             speed = 300
         }
     } else {
         cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
     }
-    basic.pause(150)
+    basic.pause(500)
 })
