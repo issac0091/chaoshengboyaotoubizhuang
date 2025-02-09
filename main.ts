1ffb49a73122e0cce426b1a02f1738c482b41671
issac0091 <yangchennk@mail.nankai.edu.cn>
2025-02-09 11:22:47 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,70 @@
+serial.redirectToUSB()
+let D1 = 0
+let D2 = 0
+let D3 = 0
+let speed = 300
+cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 70)
+cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Stop, 0)
+basic.showIcon(IconNames.Happy)
+while (!(input.buttonIsPressed(Button.A))) {
+    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Stop, 0)
+}
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Stop, 0)
+    basic.pause(100)
+    cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 70)
+    basic.pause(100)
+    D1 = Math.floor(cbit_小车类.Ultrasonic_Car())
+    while (D1 == 0) {
+        D1 = Math.floor(cbit_小车类.Ultrasonic_Car())
+    }
+    cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 130)
+    basic.pause(100)
+    D2 = Math.floor(cbit_小车类.Ultrasonic_Car())
+    while (D2 == 0) {
+        D2 = Math.floor(cbit_小车类.Ultrasonic_Car())
+    }
+    cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 30)
+    basic.pause(100)
+    D3 = Math.floor(cbit_小车类.Ultrasonic_Car())
+    while (D3 == 0) {
+        D3 = Math.floor(cbit_小车类.Ultrasonic_Car())
+    }
+    cbit_小车类.Servo_Car(cbit_小车类.enServo.S1, 70)
+    basic.pause(100)
+    if (D1 < 20 && D3 - D1 > 0) {
+        speed = speed - 50
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Right, speed)
+        speed = 300
+    } else if (D3 < 20 && D1 - D3 > 0) {
+        speed = speed - 50
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Left, speed)
+        speed = 300
+    } else if (D2 < 15) {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, speed)
+        basic.pause(150)
+        if (D3 - D1 > 0) {
+            speed = speed - 50
+            cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Right, speed)
+            speed = 300
+        } else {
+            speed = speed - 50
+            cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Left, speed)
+            speed = 300
+        }
+    } else {
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, speed)
+    }
+    basic.pause(150)
+})
